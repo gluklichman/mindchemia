67d3fc74eb71b779d03dbe5741a6dfc4fd6285cf
yurag <yura@game-pie.com>
2023-09-30 15:46:14 +0200

    add configs

---
+++ Assets/Scripts/Configs/WordConfig.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class WordConfig
+{
+    [Serializable]
+    public class WordConfigData
+    {
+        public WordData[] good_words;
+        public WordData[] bad_words;
+        public MatchRuleData[] match_rules;
+        public AnnihilationRuleData[] annihilation_rules;
+    }
+
+    [Serializable]
+    public class WordData
+    {
+        public string id;
+        public string text;
+    }
+
+    [Serializable]
+    public class MatchRuleData
+    {
+        public string id1;
+        public string id2;
+        public string result;
+    }
+
+    [Serializable]
+    public class AnnihilationRuleData
+    {
+        public string good;
+        public string bad;
+    }
+
+    private WordConfigData _data;
+
+    public WordConfig()
+    {
+        TextAsset configAsset = Resources.Load("Configs/words_config") as TextAsset;
+        _data = JsonUtility.FromJson<WordConfigData>(configAsset.text);
+        Debug.Log("config loaded");
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            WordConfig config = new WordConfig();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
